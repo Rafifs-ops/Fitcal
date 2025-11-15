806377689a4d756a8dfb855b7aac7cff28f25f53
Rafifs-ops <rafifebrians150206@gmail.com>
2025-11-16 00:49:53 +0700

    Update API URL to Render

--- src/stores/payment.js
+++ src/stores/payment.js
@@ -3,7 +3,7 @@ import { defineStore } from "pinia";
 export const usePayment = defineStore("payment", () => {
     // URL ini mengarah ke server Express.js yang Anda buat di server.js
     // Pastikan port-nya sama (misal: 3000)
-    const createTransactionUrl = "http://localhost:3000/api/create-transaction";
+    const createTransactionUrl = "https://fitcal-backend.onrender.com";
 
     // Hanya return URL-nya
     return { createTransactionUrl };
