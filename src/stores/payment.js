739ebe550489e16f0dcd405e79448b0b4b94f55b
Rafifs-ops <rafifebrians150206@gmail.com>
2025-11-16 01:03:00 +0700

    ppp

--- src/stores/payment.js
+++ src/stores/payment.js
@@ -3,7 +3,7 @@ import { defineStore } from "pinia";
 export const usePayment = defineStore("payment", () => {
     // URL ini mengarah ke server Express.js yang Anda buat di server.js
     // Pastikan port-nya sama (misal: 3000)
-    const createTransactionUrl = "https://fitcal-backend.onrender.com";
+    const createTransactionUrl = "https://fitcal-backend.onrender.com/api/create-transaction";
 
     // Hanya return URL-nya
     return { createTransactionUrl };
